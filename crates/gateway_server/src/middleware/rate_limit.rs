416f91071e5d154d7a78db8cca989e3c300f5c79
Rohit <rohitnarnolia88@gmail.com>
2026-02-15 07:45:20 +0530

    add: header utility function and better response type using snapshots

--- crates/gateway_server/src/middleware/rate_limit.rs
+++ crates/gateway_server/src/middleware/rate_limit.rs
@@ -1,16 +1,35 @@
 use std::{net::SocketAddr, time::Instant};
 
-use crate::AppState;
+use crate::{AppState, http::errors::RateLimitHttpError};
 use axum::{
     body::Body,
     extract::{ConnectInfo, State},
     http::Request,
     middleware::Next,
-    response::Response,
+    response::{IntoResponse, Response},
 };
 
+use gateway_core::rate_limiter::token_bucket::BucketState;
 use reqwest::StatusCode;
 
+fn attach_headers(response: &mut Response<Body>, snapshot: &BucketState) {
+    let headers = response.headers_mut();
+
+    headers.insert(
+        "X-RateLimit-Limit",
+        snapshot.limit.to_string().parse().unwrap(),
+    );
+
+    headers.insert(
+        "X-RateLimit-Remaining",
+        snapshot.remaining.to_string().parse().unwrap(),
+    );
+
+    headers.insert(
+        "X-RateLimit-Reset",
+        snapshot.reset_after.as_secs().to_string().parse().unwrap(),
+    );
+}
 pub async fn rate_limit_middleware(
     State(state): State<AppState>,
     ConnectInfo(addr): ConnectInfo<SocketAddr>,
@@ -18,21 +37,26 @@ pub async fn rate_limit_middleware(
     next: Next,
 ) -> Response {
     let ip = addr.ip();
-    println!("{}", ip);
 
     match state.rate_limiters.check(ip, Instant::now()) {
-        Ok(_) => next.run(req).await,
+        Ok(snapshot) => {
+            let mut response = next.run(req).await;
+            // attach headers
+            tracing::info!(ip = %ip, remaining = snapshot.remaining, "rate limit check");
+            attach_headers(&mut response, &snapshot);
+
+            response
+        }
+
         Err(err) => {
-            let retry_after_secs = err.retry_after.as_millis();
-            Response::builder()
-                .status(StatusCode::TOO_MANY_REQUESTS)
-                .header("Retry-After", retry_after_secs.to_string())
-                .header("Content-Type", "application/json")
-                .body(Body::from(format!(
-                    r#"{{"error":"rate_limited","retry_after":{}}}"#,
-                    retry_after_secs
-                )))
-                .unwrap()
+            let mut response = RateLimitHttpError {
+                retry_after_ms: err.retry_after.as_millis() as u64,
+            }
+            .into_response();
+            // attach headers
+            tracing::info!(ip = %ip, remaining = err.snapshot.remaining, "rate limit check");
+            attach_headers(&mut response, &err.snapshot);
+            response
         }
     }
 }
