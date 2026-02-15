986a891c8df043f755969a9327b6c8a961fadaa6
Rohit <rohitnarnolia88@gmail.com>
2026-02-15 12:34:07 +0530

    refactor: error limit response

--- crates/gateway_server/src/middleware/rate_limit.rs
+++ crates/gateway_server/src/middleware/rate_limit.rs
@@ -9,27 +9,34 @@ use axum::{
     response::{IntoResponse, Response},
 };
 
-use gateway_core::rate_limiter::token_bucket::BucketState;
+use gateway_core::rate_limiter::{rate_limiter::RateLimitError, token_bucket::BucketState};
 use reqwest::StatusCode;
 
 fn attach_headers(response: &mut Response<Body>, snapshot: &BucketState) {
     let headers = response.headers_mut();
-
     headers.insert(
         "X-RateLimit-Limit",
         snapshot.limit.to_string().parse().unwrap(),
     );
-
     headers.insert(
         "X-RateLimit-Remaining",
         snapshot.remaining.to_string().parse().unwrap(),
     );
-
     headers.insert(
         "X-RateLimit-Reset",
         snapshot.reset_after.as_secs().to_string().parse().unwrap(),
     );
 }
+
+fn rate_limit_response(err: RateLimitError) -> Response {
+    let mut response = RateLimitHttpError {
+        retry_after_ms: err.retry_after.as_millis() as u64,
+    }
+    .into_response();
+    attach_headers(&mut response, &err.snapshot);
+    return response;
+}
+
 pub async fn rate_limit_middleware(
     State(state): State<AppState>,
     ConnectInfo(addr): ConnectInfo<SocketAddr>,
@@ -37,26 +44,32 @@ pub async fn rate_limit_middleware(
     next: Next,
 ) -> Response {
     let ip = addr.ip();
+    let route = req.uri().path().to_string();
+    let now = Instant::now();
 
-    match state.rate_limiters.check(ip, Instant::now()) {
-        Ok(snapshot) => {
-            let mut response = next.run(req).await;
-            // attach headers
-            tracing::info!(ip = %ip, remaining = snapshot.remaining, "rate limit check");
-            attach_headers(&mut response, &snapshot);
+    let global_snapshot = match state.global_limiter.check((), now) {
+        Ok(snapshot) => snapshot,
+        Err(err) => return rate_limit_response(err),
+    };
 
-            response
-        }
-
+    let route_snapshot = match state.route_limiter.check(route.clone(), now) {
+        Ok(snapshot) => snapshot,
         Err(err) => {
-            let mut response = RateLimitHttpError {
-                retry_after_ms: err.retry_after.as_millis() as u64,
-            }
-            .into_response();
-            // attach headers
-            tracing::info!(ip = %ip, remaining = err.snapshot.remaining, "rate limit check");
-            attach_headers(&mut response, &err.snapshot);
-            response
+            tracing::warn!(ip = %ip, route = %route, "rate limit exceeded");
+            return rate_limit_response(err);
         }
-    }
+    };
+
+    let ip_snapshot = match state.ip_limiter.check(ip, now) {
+        Ok(snapshot) => snapshot,
+        Err(err) => {
+            tracing::warn!(ip = %ip, route = %route, "rate limit exceeded");
+            return rate_limit_response(err);
+        }
+    };
+
+    let mut response = next.run(req).await;
+    attach_headers(&mut response, &ip_snapshot);
+
+    response
 }
