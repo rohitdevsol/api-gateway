89b7dcdeda860d782127cd2fcddbf63c96666a38
Rohit <rohitnarnolia88@gmail.com>
2026-02-18 04:22:38 +0530

    fix: generic mismatch

--- crates/gateway_core/src/rate_limiter/rate_limiter.rs
+++ crates/gateway_core/src/rate_limiter/rate_limiter.rs
@@ -1,4 +1,4 @@
-use crate::rate_limiter::{AllowResult, TokenBucket, token_bucket::BucketState};
+use crate::rate_limiter::algorithm::{AllowResult, BucketState, RateLimitAlgorithm};
 use dashmap::DashMap;
 use std::{
     net::IpAddr,
@@ -12,15 +12,19 @@ pub struct RateLimitError {
 }
 
 #[derive(Clone)]
-pub struct RateLimiter<K> {
-    buckets: Arc<DashMap<K, TokenBucket>>,
+pub struct RateLimiter<K, A>
+where
+    A: RateLimitAlgorithm,
+{
+    buckets: Arc<DashMap<K, A>>,
     capacity: u128,
     refill_rate: u128,
 }
 
-impl<K> RateLimiter<K>
+impl<K, A> RateLimiter<K, A>
 where
-    K: std::cmp::Eq + std::hash::Hash,
+    K: Eq + std::hash::Hash,
+    A: RateLimitAlgorithm,
 {
     pub fn new(capacity: u128, refill_rate: u128) -> Self {
         Self {
@@ -29,32 +33,28 @@ where
             refill_rate,
         }
     }
+
     pub fn check(&self, key: K, now: Instant) -> Result<BucketState, RateLimitError> {
         let mut bucket = self
             .buckets
             .entry(key)
-            .or_insert_with(|| TokenBucket::new(self.capacity, self.refill_rate, now));
+            .or_insert_with(|| A::new(self.capacity, self.refill_rate, now));
 
-        bucket.last_seen = now; //update the last seen here..so TokenBucket remains agnostic
+        bucket.set_last_seen(now);
 
         match bucket.allow(now) {
-            AllowResult::Allowed => return Ok(bucket.state(now)),
-            AllowResult::Denied { retry_after } => {
-                return Err(RateLimitError {
-                    retry_after,
-                    snapshot: bucket.state(now),
-                });
-            }
+            AllowResult::Allowed => Ok(bucket.state(now)),
+            AllowResult::Denied { retry_after } => Err(RateLimitError {
+                retry_after,
+                snapshot: bucket.state(now),
+            }),
         }
     }
 
     pub fn cleanup(&self, ttl: Duration) {
-        println!("::[BUCKET_COUNT]:: Before Cleanup: {}", self.buckets.len());
-
         let now = Instant::now();
-        self.buckets.retain(|_, bucket| {
-            return now.duration_since(bucket.last_seen) <= ttl;
-        });
-        println!("::[BUCKET_COUNT]:: After Cleanup: {}", self.buckets.len());
+
+        self.buckets
+            .retain(|_, bucket| now.duration_since(bucket.last_seen()) <= ttl);
     }
 }
